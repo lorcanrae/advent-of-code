7e1c97e415a58ca164a67b7615edbab0dfbe92b9
Lorcan Rae <lorcanrae@outlook.com>
2022-12-11 03:16:00 +0000

    packaged code

--- aoc-2022/challenges/10/10.py
+++ aoc-2022/challenges/10/10.py
@@ -1,12 +1,57 @@
 def get_data():
     with open('input.txt', 'r') as f:
-        pass
+        lines = [line.strip('\n') for line in f]
+    return lines
 
-def part1(data):
-    pass
+def part1(lines):
+    cycle = 0
+    saved_signals = []
+    X = 1
 
-def part2(data):
-    pass
+    for line in lines:
+        if line == 'noop':
+            cycle += 1
+            if (cycle - 20) % 40 == 0:
+                saved_signals.append(X * cycle)
+        else:
+            for i in range(2):
+                cycle += 1
+                if (cycle - 20) % 40 == 0:
+                    saved_signals.append(X * cycle)
+            X += int(line.split()[1])
+
+    print(sum(saved_signals))
+    return sum(saved_signals)
+
+def part2(lines):
+    X = 1
+    crt_pos = 0
+    crt_out = ''
+
+    for line in lines:
+        if line == 'noop':
+            crt_out += '#' if crt_pos in [X-1, X, X+1] else '.'
+
+            if crt_pos == 39:
+                print(crt_out)
+                crt_out = ''
+                crt_pos = 0
+            else:
+                crt_pos += 1
+
+        else:
+            for i in range(2):
+                crt_out += '#' if crt_pos in [X-1, X, X+1] else '.'
+
+                if crt_pos == 39:
+                    print(crt_out)
+                    crt_out = ''
+                    crt_pos = 0
+                else:
+                    crt_pos += 1
+            X += int(line.split()[1])
+
 
 if __name__ == '__main__':
-    pass
+    part1(get_data())
+    part2(get_data())
