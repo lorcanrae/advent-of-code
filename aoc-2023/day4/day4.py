fb8b151a4fbe5d8cccea432cfbc7ff07f32cc056
Lorcan Rae <lorcanrae@gmail.com>
2023-12-04 18:25:31 +0000

    refactored to run time < 0.001 of a second for d4-c2

--- aoc-2023/day4/day4.py
+++ aoc-2023/day4/day4.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def read_data(path_: str) -> list:
 
     with open(path_, "r") as f:
@@ -11,40 +14,47 @@ def read_data(path_: str) -> list:
 
     return out_data
 
+
 def c1(data):
     score = 0
     for win_nums, game_nums in data:
-        game_win_count = 0
-        for num in win_nums:
-            if num in game_nums:
-                game_win_count += 1
+        game_win_count = len(set(win_nums).intersection(set(game_nums)))
         match game_win_count:
-            case 0: score += 0
-            case 1: score += 1
-            case _: score += 2 ** (game_win_count - 1)
+            case 0:
+                score += 0
+            case 1:
+                score += 1
+            case _:
+                score += 2 ** (game_win_count - 1)
     print(score)
     return score
 
+
 def c2(data):
     """Really not a very efficient solution"""
-    # Create list to store the number of runs per game, defaul 1 for every game
+    t1 = datetime.now()
+    # Create list to store the number of runs per game, default 1 for every game
     runs_per_game = [1] * len(data)
 
-    #
+    # Iterate over each game
     for game_number, game_runs in enumerate(runs_per_game):
         winning_numbers, game_numbers = data[game_number]
-        # Repeat experiment for the number of runs
-        for _ in range(game_runs):
-            game_win_count = len(set(winning_numbers).intersection(set(game_numbers)))
-            if game_win_count > 0:
-                for i in range(game_number+1, game_number + game_win_count+1):
-                    if i < len(data):
-                        runs_per_game[i] += 1
 
+        # length of intersect to find number of wins - may be inefficient
+        game_win_count = len(set(winning_numbers).intersection(set(game_numbers)))
 
+        # if the game had winners, iterate over future to add wins
+        if game_win_count > 0:
+            for i in range(game_number + 1, game_number + game_win_count + 1):
+                # Check to see if at the end of the list
+                if i < len(data):
+                    # Add number of runs to the runs_per_game - it's commutative
+                    runs_per_game[i] += 1 * game_runs
+
+    # Sum total number of runs/scratch cards
     total_runs = sum(runs_per_game)
-    print(runs_per_game)
     print(total_runs)
+    print(datetime.now() - t1)
     return total_runs
 
 
