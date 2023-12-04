b124b423ca957fdb624bbb3f8a68bb79b60c00b5
Lorcan Rae <lorcanrae@gmail.com>
2023-12-04 17:45:57 +0000

    replaced looping lists with set intersection

--- aoc-2023/day4/day4.py
+++ aoc-2023/day4/day4.py
@@ -11,7 +11,6 @@ def read_data(path_: str) -> list:
 
     return out_data
 
-
 def c1(data):
     score = 0
     for win_nums, game_nums in data:
@@ -20,32 +19,28 @@ def c1(data):
             if num in game_nums:
                 game_win_count += 1
         match game_win_count:
-            case 0:
-                score += 0
-            case 1:
-                score += 1
-            case _:
-                score += 2 ** (game_win_count - 1)
+            case 0: score += 0
+            case 1: score += 1
+            case _: score += 2 ** (game_win_count - 1)
     print(score)
     return score
 
-
 def c2(data):
+    """Really not a very efficient solution"""
+    # Create list to store the number of runs per game, defaul 1 for every game
     runs_per_game = [1] * len(data)
 
-    # for game_num
-    # loop for each runn in the
+    #
     for game_number, game_runs in enumerate(runs_per_game):
         winning_numbers, game_numbers = data[game_number]
+        # Repeat experiment for the number of runs
         for _ in range(game_runs):
-            game_win_count = 0
-            for winning_number in winning_numbers:
-                if winning_number in game_numbers:
-                    game_win_count += 1
+            game_win_count = len(set(winning_numbers).intersection(set(game_numbers)))
             if game_win_count > 0:
-                for i in range(game_number + 1, game_number + game_win_count + 1):
+                for i in range(game_number+1, game_number + game_win_count+1):
                     if i < len(data):
                         runs_per_game[i] += 1
+
 
     total_runs = sum(runs_per_game)
     print(runs_per_game)
