0e02d3e4f19a5af3fb44e967e542ab112cdfb8c5
Lorcan Rae <lorcanrae@gmail.com>
2023-12-08 01:59:16 +0000

    updated input and test input file name to be day agnostic

---
+++ aoc-2023/create.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir -p "day$1"
+
+directoryName="day$1"
+pyFile="day$1.py"
+fileName1="input.txt"
+fileName2="test.txt"
+
+# Check if the folder exists
+if [ -d "$directoryName" ]; then
+    echo "Folder '$directoryName' already exists. Exiting."
+    exit 1
+else
+    # Create the folder
+    echo "Creating folder '$directoryName'."
+    mkdir "$directoryName"
+
+    # Create files inside the folder
+    echo "Creating files inside '$directoryName'."
+    touch "$directoryName/$pyFile"
+    touch "$directoryName/$fileName1"
+    touch "$directoryName/$fileName2"
+
+    echo "Folder and files created successfully."
+fi
