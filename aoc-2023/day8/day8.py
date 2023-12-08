e9f45e7e93c7a8daf58ca4822da651e696b06d78
Lorcan Rae <lorcanrae@gmail.com>
2023-12-08 16:41:43 +0000

    completed d8-c2

--- aoc-2023/day8/day8.py
+++ aoc-2023/day8/day8.py
@@ -1,4 +1,5 @@
 import re
+import math
 from pprint import pprint
 import time
 
@@ -6,10 +7,10 @@ def parse_data(path: str) -> list:
     with open(path, "r") as f:
         lines = [line.strip() for line in f.readlines() if line.strip() != ""]
 
-    instructions = lines[0]
+    instructions = [0 if c == "L" else 1 for c in lines[0]]
 
     mapping = {}
-    pattern = r"[A-Z]{3}"
+    pattern = r"[A-Z0-9]{3}"
 
     for line in lines[1:]:
         regex_ = re.findall(pattern, line)
@@ -18,40 +19,47 @@ def parse_data(path: str) -> list:
     return instructions, mapping
 
 def c1(data):
-    instructions, mapping = data
-
-    counter = 0
-
+    instructions, mappings = data
+    steps = 0
     location = "AAA"
 
     while location != "ZZZ":
-        for instruction in instructions:
-            location = mapping[location][0] if instruction == "L" else mapping[location][1]
-            counter += 1
-            if location == "ZZZ":
-                break
+        location = mappings[location][instructions[steps % len(instructions)]]
+        steps += 1
 
-    print(counter)
-    return counter
+    print(steps)
+    return steps
 
 
 def c2(data):
     instructions, mappings = data
 
     starts = [k for k in mappings.keys() if k[-1] == "A"]
+    steps_per_start = []
 
-    print(starts)
+    for start in starts:
+        location = start
+        steps = 0
 
-    # for k in mappings.keys():
-    #     start = k if k[-1] == "A" else 0
+        while location[-1] != "Z":
+            location = mappings[location][instructions[steps % len(instructions)]]
+            steps += 1
 
+        steps_per_start.append(steps)
 
+    min_cycles = math.lcm(*steps_per_start)
+    print(min_cycles)
 
+    return min_cycles
 
 
 if __name__ == "__main__":
-    data = parse_data("test2.txt")
+    data = parse_data("input.txt")
+
+    st = time.time()
+    c1(data)
+    print(f"{time.time() - st: 0.5f}")
 
     st = time.time()
     c2(data)
-    print(time.time() - st)
+    print(f"{time.time() - st: 0.5f}")
