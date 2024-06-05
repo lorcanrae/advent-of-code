d730be36a0eeabcde36881562878f37d41fbba71
Lorcan Rae <77328552+lorcanrae@users.noreply.github.com>
2024-06-05 14:29:58 +0300

    updated cleaning shell script

--- clean_input.sh
+++ clean_input.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-YEAR=$1
+REPOSITORY=$1
 FILE_NAME=$2
 
 if ! pip show git-filter-repo > /dev/null 2>&1; then
     pip install git-filter-repo
 fi
 
-git filter-repo --path-glob "aoc-$YEAR/*/$FILE_NAME" --invert-paths --force
-git push --set-upstream git@github.com:lorcanrae/advent-of-code.git main --force
-git remote add origin git@github.com:lorcanrae/advent-of-code.git
+git chechout main
+
+git filter-repo --path $FILE_NAME --invert-paths
+git push origin --force --all and git push origin --force --tags
+# git push --all --set-upstream git@github.com:lorcanrae/$REPOSITORY.git main --force
