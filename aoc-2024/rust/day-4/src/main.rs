852e4129127c94ae50357d9855e863bdeaf9b2e4
Lorcan Rae <lorcanrae@gmail.com>
2024-12-04 16:30:18 +0200

    day 4 - part 2 complete

--- aoc-2024/rust/day-4/src/main.rs
+++ aoc-2024/rust/day-4/src/main.rs
@@ -103,10 +103,55 @@ fn part_one(matrix: &Vec<Vec<char>>) -> i32 {
     counter
 }
 
+fn part_two(matrix: &Vec<Vec<char>>) -> i32 {
+    let mut counter = 0;
+    let num_rows = matrix.len();
+    let num_cols = if num_rows > 0 { matrix[0].len() } else { 0 };
+    let allowable = [vec!['M', 'A', 'S'], vec!['S', 'A', 'M']];
+
+    for i_row in 0..num_rows {
+        for j_col in 0..num_cols {
+            if matrix[i_row][j_col] == 'A'
+                && j_col + 1 < num_cols
+                && j_col >= 1
+                && i_row >= 1
+                && i_row + 1 < num_rows
+            {
+                let mut found = Vec::new();
+                let i_right = j_col + 1;
+                let i_left = j_col - 1;
+                let i_up = i_row - 1;
+                let i_down = i_row + 1;
+
+                // Down Left -> Up Right
+                found.push(vec![
+                    matrix[i_down][i_left],
+                    matrix[i_row][j_col],
+                    matrix[i_up][i_right],
+                ]);
+
+                // Up Left -> Down Right
+                found.push(vec![
+                    matrix[i_up][i_left],
+                    matrix[i_row][j_col],
+                    matrix[i_down][i_right],
+                ]);
+
+                if allowable.contains(&found[0]) && allowable.contains(&found[1]) {
+                    counter += 1;
+                };
+            }
+        }
+    }
+    println!("Found: {counter}");
+    counter
+}
+
 fn main() {
     match parse_input() {
         Ok(matrix) => {
             part_one(&matrix);
+            part_two(&matrix);
         }
         Err(e) => {
             eprintln!("Error opening file: {e}");
