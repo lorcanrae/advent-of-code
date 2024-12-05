76e82d452f164de6332ac269d14bd31e67e645e9
Lorcan Rae <lorcanrae@gmail.com>
2024-12-05 16:24:14 +0200

    day 5 - part 2 complete

--- aoc-2024/rust/day-5/src/main.rs
+++ aoc-2024/rust/day-5/src/main.rs
@@ -30,9 +30,6 @@ fn parse_input() -> Result<ParsedData> {
                 .collect()
         })
         .collect();
-    // println!("{rules:?}");
-    // println!("{pages:?}");
-
     Ok(ParsedData { rules, pages })
 }
 
@@ -41,14 +38,14 @@ fn part_one(data: &ParsedData) -> i32 {
 
     for page_set in &data.pages {
         let mut valid = true;
-        for i in 0..page_set.len() {
-            if i == 0 {
-                continue;
-            }
+        let l = page_set.len();
 
-            if i != 0 {
-                let prev_i = i - 1;
-                let pair = page_set[prev_i..=i].to_vec();
+        for i in 0..l {
+            if i == l - 1 {
+                break;
+            }
+            if i < l {
+                let pair = vec![page_set[i], page_set[i + 1]];
                 if data.rules.contains(&pair) {
                     continue;
                 }
@@ -56,71 +53,40 @@ fn part_one(data: &ParsedData) -> i32 {
             valid = false;
         }
 
-        sum += if valid {
-            page_set[page_set.len() / 2]
-        } else {
-            0
-        };
+        sum += if valid { page_set[l / 2] } else { 0 };
     }
     println!("Sum: {sum}");
     sum
 }
 
 fn part_two(data: &ParsedData) -> i32 {
-    let mut sum_valid = 0;
     let mut sum_corrected = 0;
 
     for page_set in &data.pages {
-        let mut valid = true;
         let mut corrected = false;
         let mut corrected_pages = page_set.clone();
-        let mut broken = false;
+        let l = corrected_pages.len();
 
-        for i in 0..corrected_pages.len() {
-            if i == 0 {
-                continue;
-            }
-
-            let prev_i = i - 1;
-            if i != 0 {
-                let pair = corrected_pages[prev_i..=i].to_vec();
-                if data.rules.contains(&pair) {
-                    continue;
-                }
-            }
-            valid = false;
-
-            if !valid {
-                let swap = vec![corrected_pages[i], corrected_pages[prev_i]];
-                if data.rules.contains(&swap) {
-                    corrected_pages.swap(i, prev_i);
+        for i in 0..l {
+            for j in i + 1..l {
+                let switcheroo = vec![corrected_pages[j], corrected_pages[i]];
+                if data.rules.contains(&switcheroo) {
+                    corrected_pages.swap(i, j);
                     corrected = true;
-                } else {
-                    broken = true;
                 }
             }
         }
 
-        if broken {
-            println!("Broken: {corrected_pages:?}");
-        }
-
-        if valid {
-            sum_valid += page_set[page_set.len() / 2]
-        } else if corrected {
-            println!("{corrected_pages:?}");
-            sum_corrected += corrected_pages[corrected_pages.len() / 2]
-        };
+        sum_corrected += if corrected { corrected_pages[l / 2] } else { 0 };
     }
-    println!("Sum valid: {sum_valid}");
     println!("Sum corrected: {sum_corrected}");
-    sum_valid
+    sum_corrected
 }
 
 fn main() {
     match parse_input() {
         Ok(data) => {
-            // part_one(&data);
+            part_one(&data);
             part_two(&data);
         }
         Err(e) => println!("Error opening file: {e}"),
