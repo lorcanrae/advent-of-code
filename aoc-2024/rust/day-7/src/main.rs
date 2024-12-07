65c9ea039edb470d5b556aff00cd56ead2e090bd
Lorcan Rae <lorcanrae@gmail.com>
2024-12-07 16:37:54 +0200

    day 7 - part 2

--- aoc-2024/rust/day-7/src/main.rs
+++ aoc-2024/rust/day-7/src/main.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 use nom::bytes::complete::tag;
 use nom::character::complete::{self, newline, space1};
 use nom::multi::separated_list1;
-use nom::sequence::{self, separated_pair};
+use nom::sequence::separated_pair;
 use nom::IResult;
 use std::env;
 use std::fs;
@@ -19,34 +19,63 @@ fn parse_input(data: &str) -> IResult<&str, Vec<(u64, Vec<u64>)>> {
     )(data)
 }
 
-fn part_one(data: Vec<(u64, Vec<u64>)>) -> Result<String> {
+fn part_one(data: &Vec<(u64, Vec<u64>)>) -> Result<String> {
     let data = data.clone();
 
-    let result: u64 = data
+    let accumulation: u64 = data
         .iter()
         .filter_map(|(target, sequence)| {
             let operator_count = sequence.len() - 1;
-            dbg!(operator_count);
             (0..operator_count)
                 .map(|_| vec!['+', '*'])
                 .multi_cartesian_product()
                 .any(|permutation| {
                     let mut operation = permutation.iter();
-                    *target
-                        == sequence
-                            .iter()
-                            .copied()
-                            .reduce(|acc, next| match operation.next().unwrap() {
-                                '+' => acc + next,
-                                '*' => acc * next,
-                                _ => panic!("Invalid operation"),
-                            })
-                            .unwrap()
+                    let result = sequence
+                        .iter()
+                        .copied()
+                        .reduce(|acc, next| match operation.next().unwrap() {
+                            '+' => acc + next,
+                            '*' => acc * next,
+                            _ => panic!("Invalid operation"),
+                        })
+                        .unwrap();
+                    *target == result
                 })
                 .then_some(target)
         })
         .sum();
-    Ok(result.to_string())
+    Ok(accumulation.to_string())
+}
+
+fn part_two(data: &Vec<(u64, Vec<u64>)>) -> Result<String> {
+    let data = data.clone();
+
+    let accumulation: u64 = data
+        .iter()
+        .filter_map(|(target, sequence)| {
+            let operator_count = sequence.len() - 1;
+            (0..operator_count)
+                .map(|_| vec!['+', '*', '|'])
+                .multi_cartesian_product()
+                .any(|permutation| {
+                    let mut operation = permutation.iter();
+                    let result = sequence
+                        .iter()
+                        .copied()
+                        .reduce(|acc, next| match operation.next().unwrap() {
+                            '+' => acc + next,
+                            '*' => acc * next,
+                            '|' => format!("{}{}", acc, next).parse::<u64>().unwrap(),
+                            _ => panic!("Invalid operation"),
+                        })
+                        .unwrap();
+                    *target == result
+                })
+                .then_some(target)
+        })
+        .sum();
+    Ok(accumulation.to_string())
 }
 
 fn main() -> Result<()> {
@@ -54,16 +83,15 @@ fn main() -> Result<()> {
     let file_name = args[1].to_string();
     let data = fs::read_to_string(file_name)?;
 
-    // let (_, parsed_data) = parse_input(data)?;
     match parse_input(&data) {
         Ok((_, parsed_data)) => {
-            let p1 = part_one(parsed_data);
-            println!("{p1:?}");
+            let p1 = part_one(&parsed_data)?;
+            println!("{p1}");
+            let p2 = part_two(&parsed_data)?;
+            println!("{p2}");
         }
         Err(e) => eprintln!("{e}"),
     }
-
-    // dbg!(parsed_data);
 
     Ok(())
 }
