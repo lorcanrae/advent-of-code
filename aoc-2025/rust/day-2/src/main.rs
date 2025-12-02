d3716e643f6578d7dbd9d23ab08b101356f99674
Lorcan Rae <lorcanrae@gmail.com>
2025-12-02 14:17:55 +0200

    AoC 2025: day 2 part 1

---
+++ aoc-2025/rust/day-2/src/main.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use std::fs;
+
+fn parse_data(path: &str) -> Result<Vec<(i64, i64)>> {
+    let input_data: String = fs::read_to_string(path)?;
+    let data: Vec<(i64, i64)> = input_data
+        .trim()
+        .split(",")
+        .map(|line| {
+            dbg!(line);
+            let (start, end) = line.split_once("-").unwrap_or_default();
+            let start = start.parse::<i64>().unwrap();
+            let end = end.parse::<i64>().unwrap();
+            (start, end)
+        })
+        .collect();
+    Ok(data)
+}
+
+fn part_one(data: &[(i64, i64)]) -> Result<String> {
+    let invalid_ids: Vec<i64> = data
+        .iter()
+        .flat_map(|(start, end)| *start..=*end)
+        .filter(|&num| {
+            let stringed = num.to_string();
+            if stringed.len() % 2 != 0 {
+                return false;
+            };
+            let (a, b) = stringed.split_at(stringed.len() / 2);
+            a == b
+        })
+        .collect();
+
+    let result: i64 = invalid_ids.iter().sum();
+    Ok(result.to_string())
+}
+
+fn main() -> Result<()> {
+    let file_path = "inputs/input.txt";
+    let data = parse_data(file_path)?;
+
+    let p1 = part_one(&data)?;
+    println!("p1 solution: {p1}");
+
+    Ok(())
+}
