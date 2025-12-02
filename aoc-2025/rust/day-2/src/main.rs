c0c5c2de7a23e6e4a0ba4e70fb70cc1f569d5d43
Lorcan Rae <lorcanrae@gmail.com>
2025-12-02 14:37:07 +0200

    AoC 2025: day 2 part 2

--- aoc-2025/rust/day-2/src/main.rs
+++ aoc-2025/rust/day-2/src/main.rs
@@ -7,7 +7,6 @@ fn parse_data(path: &str) -> Result<Vec<(i64, i64)>> {
         .trim()
         .split(",")
         .map(|line| {
-            dbg!(line);
             let (start, end) = line.split_once("-").unwrap_or_default();
             let start = start.parse::<i64>().unwrap();
             let end = end.parse::<i64>().unwrap();
@@ -35,12 +34,45 @@ fn part_one(data: &[(i64, i64)]) -> Result<String> {
     Ok(result.to_string())
 }
 
+fn part_two(data: &[(i64, i64)]) -> Result<String> {
+    let invalid_ids: Vec<i64> = data
+        .iter()
+        .flat_map(|(start, end)| *start..=*end)
+        .filter(|&num| {
+            let stringed = num.to_string();
+            let len = stringed.len();
+
+            // Start from largest potential pattern and get smaller
+            for pattern_len in (1..=len / 2).rev() {
+                // Skip pattern lengths that don't modulo
+                if len % pattern_len != 0 {
+                    continue;
+                }
+                // Slice a section, repeat to fit length, compare
+                let pattern = &stringed[..pattern_len];
+                let repeated = pattern.repeat(len / pattern_len);
+                if repeated == stringed {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .collect();
+
+    let result: i64 = invalid_ids.iter().sum();
+    Ok(result.to_string())
+}
+
 fn main() -> Result<()> {
     let file_path = "inputs/input.txt";
     let data = parse_data(file_path)?;
 
     let p1 = part_one(&data)?;
     println!("p1 solution: {p1}");
+
+    let p2 = part_two(&data)?;
+    println!("p2 solution: {p2}");
 
     Ok(())
 }
