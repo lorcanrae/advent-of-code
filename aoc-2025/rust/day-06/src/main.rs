dbfafe5b26565f74a8bca66c89aa94417eaba654
Lorcan Rae <lorcanrae@gmail.com>
2025-12-07 12:39:24 +0200

    AoC 2025 - day 6 part 2, reverse parse of grid

--- aoc-2025/rust/day-06/src/main.rs
+++ aoc-2025/rust/day-06/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::{Result, anyhow};
 use std::{fs, time::Instant};
 
-fn parse_one(path: &str) -> Result<(Vec<Vec<u64>>, Vec<String>)> {
+fn parse(path: &str) -> Result<(Vec<Vec<u64>>, Vec<String>)> {
     let raw = fs::read_to_string(path)?;
     let mut lines: Vec<&str> = raw.lines().collect();
 
@@ -24,7 +24,7 @@ fn parse_one(path: &str) -> Result<(Vec<Vec<u64>>, Vec<String>)> {
 }
 
 fn part_one(path: &str) -> Result<String> {
-    let (nums, ops) = parse_one(path)?;
+    let (nums, ops) = parse(path)?;
 
     let transposed: Vec<Vec<u64>> = (0..nums[0].len())
         .map(|col_idx| nums.iter().map(|row| row[col_idx]).collect())
@@ -45,71 +45,134 @@ fn part_one(path: &str) -> Result<String> {
     Ok(total.to_string())
 }
 
-fn parse_two(path: &str) -> Result<(Vec<Vec<u64>>, Vec<String>)> {
+fn part_two_b(path: &str) -> Result<String> {
+    let raw = fs::read_to_string(path)?;
+
+    let lines: Vec<String> = raw.lines().map(|line| line.to_string()).collect();
+
+    let num_rows = lines.len() - 1;
+    let max_line_len = lines.iter().map(|line| line.len()).max().unwrap_or(0);
+
+    let char_grid: Vec<Vec<char>> = lines
+        .iter()
+        .map(|line| {
+            let mut chars: Vec<char> = line.chars().collect();
+            chars.resize(max_line_len, ' ');
+            chars
+        })
+        .collect();
+
+    let mut buffer: Vec<u64> = Vec::new();
+    let mut op = " ";
+    let mut total: u64 = 0;
+
+    for col_idx in (0..max_line_len).rev() {
+        // check to see if at a column break
+        let is_separator = char_grid.iter().all(|row| row[col_idx] == ' ');
+
+        if is_separator && !buffer.is_empty() {
+            // at a column break, do math, clear buffer
+            match op {
+                "+" => total += buffer.iter().sum::<u64>(),
+                "*" => total += buffer.iter().product::<u64>(),
+                _ => {}
+            }
+            buffer.clear();
+            op = " ";
+        } else if !is_separator {
+            // get the val and push it to a buffer
+            if let Ok(val) = char_grid[..num_rows]
+                .iter()
+                .map(|row| row[col_idx])
+                .collect::<String>()
+                .trim()
+                .parse()
+            {
+                buffer.push(val);
+            };
+        }
+
+        // Get the op and push to buffer
+        match char_grid[num_rows][col_idx] {
+            '+' => op = "+",
+            '*' => op = "*",
+            _ => {}
+        }
+    }
+
+    // last val
+    if !buffer.is_empty() {
+        match op {
+            "+" => total += buffer.iter().sum::<u64>(),
+            "*" => total += buffer.iter().product::<u64>(),
+            _ => panic!(),
+        }
+    }
+
+    Ok(total.to_string())
+}
+
+fn part_two(path: &str) -> Result<String> {
     let raw = fs::read_to_string(path)?;
     let mut lines: Vec<String> = raw.lines().map(|line| line.to_string()).collect();
-    // let mut lines: Vec<&str> = raw.lines().collect();
 
-    let ops_line = lines.pop().ok_or_else(|| anyhow!("empty"))?;
-    let ops: Vec<String> = ops_line
+    let ops: Vec<String> = lines
+        .pop()
+        .ok_or_else(|| anyhow!("empty"))?
         .split_ascii_whitespace()
         .map(|c| c.to_string())
         .collect();
 
-    let max_line_length = lines.iter().map(|line| line.len()).max().unwrap_or(0);
+    let max_line_len = lines.iter().map(|line| line.len()).max().unwrap_or(0);
 
-    lines.iter_mut().for_each(|line| {
-        if line.len() < max_line_length {
-            let padding = " ".repeat(max_line_length - line.len());
-            line.push_str(&padding);
-        }
-    });
-
-    let break_points: Vec<usize> = (0..max_line_length)
-        .filter(|&col_index| {
-            lines
-                .iter()
-                .all(|row| row.chars().nth(col_index).map_or(true, |c| c == ' '))
+    // convert to grid and pad
+    let char_grid: Vec<Vec<char>> = lines
+        .iter()
+        .map(|line| {
+            let mut chars: Vec<char> = line.chars().collect();
+            chars.resize(max_line_len, ' ');
+            chars
         })
         .collect();
 
+    // lambda for col of whitespaces
+    let is_separator = |col_idx: usize| -> bool { char_grid.iter().all(|row| row[col_idx] == ' ') };
+
+    // vec of tuples for individual columns
     let mut groups: Vec<(usize, usize)> = Vec::new();
     let mut start = 0;
-
-    for bp in break_points {
-        if bp > start {
-            groups.push((start, bp));
+    for col in 0..max_line_len {
+        if is_separator(col) {
+            if col > start {
+                groups.push((start, col));
+            }
+            start = col + 1;
         }
-        start = bp + 1;
     }
 
-    // last one
-    if start < max_line_length {
-        groups.push((start, max_line_length));
+    // Don't forget the last one
+    if start < max_line_len {
+        groups.push((start, max_line_len));
     }
 
+    // parse grid to useable values
     let nums: Vec<Vec<u64>> = groups
         .iter()
         .map(|(start, end)| {
             (*start..*end)
-                .rev()
+                .rev() // right to left
                 .map(|col_idx| {
-                    let num_str: String = lines
+                    char_grid
                         .iter()
-                        .filter_map(|row| row.chars().nth(col_idx))
-                        .collect();
-
-                    num_str.trim().parse::<u64>().ok().unwrap()
+                        .map(|row| row[col_idx])
+                        .collect::<String>()
+                        .trim()
+                        .parse()
+                        .expect("invalid number in grid")
                 })
                 .collect()
         })
         .collect();
-
-    Ok((nums, ops))
-}
-
-fn part_two(path: &str) -> Result<String> {
-    let (nums, ops) = parse_two(path)?;
 
     let total: u64 = nums
         .iter()
@@ -129,21 +192,23 @@ fn part_two(path: &str) -> Result<String> {
 fn main() -> Result<()> {
     let file_path = "inputs/input.txt";
 
-    // let data = parse(file_path)?;
-    // test p1 = 4_277_556
-
     let start = Instant::now();
     let p1 = part_one(file_path)?;
     let duration = start.elapsed();
-    println!("p1 solution: {p1} in {duration:?}");
-    // 5_381_996_914_800
+    println!("p1 solution: {p1} in {duration:?}"); // 167.914µs
+    // 5_381_996_914_800 - :check
 
     let start = Instant::now();
     let p2 = part_two(file_path)?;
     let duration = start.elapsed();
-    println!("p2 solution: {p2} in {duration:?}");
+    println!("p2 solution: {p2} in {duration:?}"); // 240.926µs
     // 3_264_322 - too low
     // 9_734_866_307_577 - too high
+
+    let start = Instant::now();
+    let p2 = part_two_b(file_path)?;
+    let duration = start.elapsed();
+    println!("p2 - reverse parse: {p2} in {duration:?}"); // 240.926µs
 
     Ok(())
 }
