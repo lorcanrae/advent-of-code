8742b421f753de5de921d93499f80bd1a561d883
Lorcan Rae <lorcanrae@gmail.com>
2025-12-12 00:10:12 +0200

    AoC 2025 - day 11 part 1 & 2

---
+++ aoc-2025/rust/day-11/src/main.rs
@@ -0,0 +1,73 @@
+use std::{collections::HashMap, time::Instant};
+
+use anyhow::Result;
+use pathfinding::prelude::count_paths;
+
+fn parse(input: &str) -> Result<HashMap<&str, Vec<&str>>> {
+    let data: HashMap<&str, Vec<&str>> = input
+        .lines()
+        .map(|line| {
+            line.split_once(':')
+                .map(|(s, e)| (s, e.split_ascii_whitespace().collect::<Vec<&str>>()))
+                .unwrap()
+        })
+        .collect();
+
+    Ok(data)
+}
+
+fn part_one(input: &str) -> Result<String> {
+    let devices = parse(input).unwrap();
+    let n = count_paths(
+        "you",
+        |device| devices[device].iter().copied(),
+        |&target| target == "out",
+    );
+
+    Ok(n.to_string())
+}
+
+// This is weak and I'm totally fine with it
+fn part_two(input: &str) -> Result<String> {
+    let mut devices = parse(input)?;
+    devices.insert("out", vec![]);
+
+    let count = |from: &str, to: &str| {
+        count_paths(
+            from,
+            |device| devices[device].iter().copied(),
+            |&target| target == to,
+        )
+    };
+
+    let [s_d, d_f, f_o, s_f, f_d, d_o] = [
+        ("svr", "dac"),
+        ("dac", "fft"),
+        ("fft", "out"),
+        ("svr", "fft"),
+        ("fft", "dac"),
+        ("dac", "out"),
+    ]
+    .map(|(from, to)| count(from, to));
+
+    // in one of the sides the value will be zero, reducing that path to zero.
+    let n_paths = (s_d * d_f * f_o) + (s_f * f_d * d_o);
+
+    Ok(n_paths.to_string())
+}
+
+fn main() -> Result<()> {
+    let data = include_str!("../inputs/input.txt");
+
+    let start = Instant::now();
+    let p1 = part_one(data)?;
+    let duration = start.elapsed();
+    println!("p1 solution: {p1} in {duration:?}"); // 321.311µs
+
+    let start = Instant::now();
+    let p2 = part_two(data)?;
+    let duration = start.elapsed();
+    println!("p2 solution: {p2} in {duration:?}");
+
+    Ok(())
+}
